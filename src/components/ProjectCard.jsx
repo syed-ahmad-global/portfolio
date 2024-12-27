ea171f053316401349b3a7cef36518e0d7096807
Ahmad Shah <101276981+syed-ahmad-global@users.noreply.github.com>
2024-12-27 19:47:37 +0500

    adds project comp

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { FiGithub, FiGlobe } from "react-icons/fi";
+
+const ProjectCard = ({ project }) => {
+  return (
+    <div
+      className="rounded-xl border bg-card text-card-foreground shadow flex flex-col"
+      style={{ borderColor: "rgb(156, 163, 175)" }}
+    >
+      <div className="flex w-full max-h-[15rem] min-h-[15rem] rounded-xl flex-col space-y-1.5 p-6 pb-2 skeleton relative">
+        <img
+          className="h-full w-full object-cover rounded-xl"
+          alt={project.title}
+          src={project.imageSrc}
+        />
+      </div>
+      <div className="p-6 pt-0 flex flex-col gap-2">
+        <div className="font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          {project.title}
+        </div>
+        <div className="prose max-w-full text-pretty text-xs text-muted-foreground dark:prose-invert">
+          <p>{project.description}</p>
+        </div>
+      </div>
+      <div className="p-6 pt-0 flex flex-col items-start justify-between gap-4">
+        <div className="mt-2 flex flex-wrap gap-1">
+          {project.technologies.map((tech, index) => (
+            <div
+              key={index}
+              className="inline-flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-transparent bg-gray-500 text-white px-1 py-0 text-[0.60rem]"
+            >
+              {tech}
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-row flex-wrap items-start gap-1">
+          {project.sourceLink ? (
+            <a
+              target="_blank"
+              href={project.sourceLink}
+              rel="noopener noreferrer"
+            >
+              <div className="flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 border-transparent bg-white text-black shadow hover:bg-gray-300 gap-2 px-2 py-1 text-xs">
+                <FiGithub className="w-3 h-3" />
+                Source
+              </div>
+            </a>
+          ) : null}
+
+          {project.websiteLink ? (
+            <a
+              target="_blank"
+              href={project.websiteLink}
+              rel="noopener noreferrer"
+            >
+              <div className="flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 border-transparent bg-white text-black shadow hover:bg-gray-300 gap-2 px-2 py-1 text-xs">
+                <FiGlobe className="w-3 h-3" />
+                Website
+              </div>
+            </a>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
-function Projects() {
-  return <section className="projects">Projects</section>;
-}
+import ProjectCard from "./ProjectCard";
+
+import projectsData from "../projectsData.json";
+
+import uranusImg from "../assets/images/projects/uranus.jpg";
+import navaImg from "../assets/images/projects/nava.jpg";
+
+const Projects = () => {
+  const projects = [
+    {
+      title: "Uranus",
+      description:
+        "Uranus is fully responsive website. Main theme of this portfolio website of a company.",
+      imageSrc: uranusImg,
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+      sourceLink: "https://github.com/syed-ahmad-global/Uranus",
+      websiteLink: "https://uranus1.netlify.app",
+    },
+    {
+      title: "Nava",
+      description:
+        "Nava is fully responsive website. Main theme of this website is renting, buying and selling of boat.",
+      imageSrc: navaImg,
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+      sourceLink: "https://github.com/syed-ahmad-global/Nava",
+      websiteLink: "https://nava1.netlify.app",
+    },
+  ];
+
+  return (
+    <div className="w-full mt-10 flex flex-col">
+      <span className="text-xl sm:text-4xl md:text-4xl lg:text-4xl font-semibold">
+        Projects
+      </span>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-10">
+        {projectsData.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
