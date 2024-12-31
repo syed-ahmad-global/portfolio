b23d106dbbe3e5dc2ecd12327536236e6f867c4d
Ahmad Shah <101276981+syed-ahmad-global@users.noreply.github.com>
2024-12-31 20:27:22 +0500

    remove unwanted code assets

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,31 +4,7 @@ import ProjectCard from "./ProjectCard";
 
 import projectsData from "../projectsData.json";
 
-import uranusImg from "../assets/images/projects/uranus.jpg";
-import navaImg from "../assets/images/projects/nava.jpg";
-
 const Projects = () => {
-  const projects = [
-    {
-      title: "Uranus",
-      description:
-        "Uranus is fully responsive website. Main theme of this portfolio website of a company.",
-      imageSrc: uranusImg,
-      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-      sourceLink: "https://github.com/syed-ahmad-global/Uranus",
-      websiteLink: "https://uranus1.netlify.app",
-    },
-    {
-      title: "Nava",
-      description:
-        "Nava is fully responsive website. Main theme of this website is renting, buying and selling of boat.",
-      imageSrc: navaImg,
-      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-      sourceLink: "https://github.com/syed-ahmad-global/Nava",
-      websiteLink: "https://nava1.netlify.app",
-    },
-  ];
-
   return (
     <div className="w-full mt-10 flex flex-col">
       <span className="text-xl sm:text-4xl md:text-4xl lg:text-4xl dark:text-custom-gray text-custom-white font-semibold">
